2983302f0fefe21402cdea5c78486caf20c22c01
tselun <tsc8@sfu.ca>
2019-06-27 17:52:03 -0700

    wasd

--- public/game.js
+++ public/game.js
@@ -18,29 +18,35 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('sky', 'sky.png');
+    //this.load.image('sky', 'sky.png');
     this.load.image('logo', 'face.jpg');
-    this.load.image('red', 'red.png');
     this.load.image('green', 'green.png');
-    // updated some codes
 }
 
 function create ()
 {
     this.add.image(400, 300, 'green');
-    this.add.image(400, 300, 'sky');
+    //this.add.image(400, 300, 'sky');
 
-    var particles = this.add.particles('red');
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
     var logo = this.physics.add.image(400, 100, 'logo');
 
-    logo.setVelocity(200, 200);
-    logo.setBounce(1, 1);
+    //logo.setVelocity(200, 200);
+    //logo.setBounce(1, 1);
     logo.setCollideWorldBounds(true);
 
-    emitter.startFollow(logo);
+    //emitter.startFollow(logo);
+}
+
+function update(){
+    this.cursors = this.input.keyboard.addKeys({
+        up:Phaser.Input.Keyboard.KeyCodes.W,
+        down:Phaser.Input.Keyboard.KeyCodes.S,
+        left:Phaser.Input.Keyboard.KeyCodes.A,
+        right:Phaser.Input.Keyboard.KeyCodes.D
+    });
+
+    if(cursors.up.isDown) player.setVelocityY(-160);
+    if(cursors.down.isDown) player.setVelocityY(160);
+    if(cursors.left.isDown) player.setVelocityX(-160);
+    if(cursors.right.isDown) player.setVelocityX(160);
 }
