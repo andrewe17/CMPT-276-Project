a5250ff94844bbecccaf3e6de7258f8300d046aa
tselun <tsc8@sfu.ca>
2019-06-27 19:58:18 -0700

    23

--- public/game.js
+++ public/game.js
@@ -34,22 +34,22 @@ function create(){
     this.add.image(400, 300, 'green');
     cursor = this.input.keyboard.createCursorKeys();
     player = this.physics.add.sprite(100, 100, 'dude');
-    this.anims.create({
+    player.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frames: player.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
         frameRate: 10,
         repeat: -1
     });
     
-    this.anims.create({
+    player.anims.create({
         key: 'turn',
         frames: [ { key: 'dude', frame: 4 } ],
         frameRate: 20
     });
     
-    this.anims.create({
+    player.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frames: player.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
         frameRate: 10,
         repeat: -1
     });
