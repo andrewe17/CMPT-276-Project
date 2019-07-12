8981fd744dd6d9c918ade5a82e67a9978db48cf0
MAGKILLER <jacklishijia@gmail.com>
2019-07-11 17:45:46 -0700

    Update game.js

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -274,7 +274,7 @@ function addPlayer(self, playerInfo) {
     self.ninja.setCollideWorldBounds(true);
     self.ninja.setVelocity(0, 0);
     self.cameras.main.startFollow(self.ninja, true, 0.05, 0.05, 0.05, 0.05);
-    //self.ninja.healthText = self.add.text(playerInfo.x, playerInfo.y, playerInfo.health, {fontFamily:'"Roboto Condensed"', fill: '#000'});
+    self.ninja.healthText = self.add.text(playerInfo.x - 16, playerInfo.y + 20, playerInfo.health, {fontFamily:'"Roboto Condensed"', fill: '#000'});
 
     self.physics.add.collider(self.ninja, wx, pb);
 }
