9f5a439fa45503c14e58fbfb0a93198119b06204
Andrew Eng <andrew_eng@shaw.ca>
2019-07-27 02:19:22 -0700

    url fix

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ io.sockets.on('connection', function(socket){
 
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
-app.post('/signin', async (req, res) => {//this updates the form when the form from login is submited
+app.post('/', async (req, res) => {//this updates the form when the form from login is submited
   try {
     const que = 'SELECT username, password FROM login WHERE EXISTS (SELECT username, password FROM login WHERE login.username = $1 AND login.password = $2);'
     const value =[req.body.user,req.body.password]
@@ -217,7 +217,7 @@ app.post('/signin', async (req, res) => {//this updates the form when the form f
 })
 
 
-app.post('/signup', async (req, res) => {//this updates the form when the form from login is submited
+app.post('/', async (req, res) => {//this updates the form when the form from login is submited
   try {
     const que = 'SELECT username FROM login WHERE EXISTS (SELECT username FROM login WHERE login.username = $1 );'
     const value =[req.body.userup];
@@ -247,11 +247,11 @@ app.post('/signup', async (req, res) => {//this updates the form when the form f
 })
 
 
-app.post('/signup1', function(req, res){
-    var results = {'results':0};
-    res.render('pages/login',results);
-});
-app.post('/login', function(req, res){
+// app.post('/signup1', function(req, res){
+//     var results = {'results':0};
+//     res.render('pages/login',results);
+// });
+app.post('/', function(req, res){
     var results = {'results':-1};
     res.render('pages/login',results);
 });
